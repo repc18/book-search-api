3d8c3e3a9e3c69414a530f67d732f54bcf3eb581
Rephael <rephaelcharis@gmail.com>
2020-11-08 22:17:22 +0800

    Add /api to routes

--- api/routes/bookSearchRoutes.js
+++ api/routes/bookSearchRoutes.js
@@ -3,20 +3,20 @@ module.exports = app => {
     const books = require("../controllers/bookSearchController");
 
     // Create a new book
-    app.post("/books", books.create);
+    app.post("/api/books", books.create);
 
     // Retrieve all books
-    app.get("/books", books.findAll);
+    app.get("/api/books", books.findAll);
 
     // Retrieve single book with book id
-    app.get("/books/:bookId", books.findOne);
+    app.get("/api/books/:bookId", books.findOne);
 
     // Update a book with book id
-    app.put("/books/:bookId", books.update);
+    app.put("/api/books/:bookId", books.update);
 
     // Delete a book with book id
-    app.delete("/books/:bookId", books.delete);
+    app.delete("/api/books/:bookId", books.delete);
 
     // Delete all books data
-    app.delete("/books", books.deleteAll);
+    app.delete("/api/books", books.deleteAll);
 };
